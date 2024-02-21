192f4850537ef2d08d889fa85d921dbd621783fb
bajorunaitej <bajorunaitej@gmail.com>
2024-02-21 12:03:18 +0200

    useEffect

---
+++ react-effect-vite/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>Labas</h1>
+      <h2>Labas</h2>
+      <h3>Labas</h3>
+      <h4>Labas</h4>
+    </>
+  );
+}
+
+export default App;
---
+++ react-effect/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>labas</h1>
+      <h3>taaip</h3>
+    </div>
+  );
+}
+
+export default App;
