e4f62d78815e66e8678f941c030623d7dfc8a79b
bajorunaitej <bajorunaitej@gmail.com>
2024-02-21 14:18:11 +0200

    testing

---
+++ testing/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-red-500">labas</h1>
+      <h2>labas</h2>
+      <button>submit</button>
+      <input type="text" placeholder="vardas" />
+    </div>
+  );
+}
