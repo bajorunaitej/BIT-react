73c34c2ff37913e0f7dc6914f3ff323cb2d7b3a0
bajorunaitej <bajorunaitej@gmail.com>
2024-02-26 23:45:38 +0200

    //

---
+++ my-scooter/src/components/Button.jsx
@@ -0,0 +1,16 @@
+export default function Button({
+  text = "Mygtukas",
+  color = "red",
+  textColor = "white",
+  onClick,
+}) {
+  return (
+    <button
+      className={`px-6 py-1 rounded-md break-keep buttonas`}
+      style={{ color: textColor, background: color }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
---
+++ my-scooter/src/components/Top.jsx
@@ -0,0 +1,17 @@
+import AddScooterForm from "./AddScooter";
+import Button from "./Button";
+
+export default function Top() {
+  return (
+    <div className="container mx-auto bg-blue-50 min-h-[400px] p-4">
+      <h2 className="text-center my-20 text-xl font-bold">
+        Paspirtuko pridėjimas
+      </h2>
+      <AddScooterForm />
+      <div className="flex justify-center mt-28 gap-4">
+        <Button text="Rodyti laisvus" color="green" hoverColor="blue" />
+        <Button color="red" text="Rodyti užimtus" />
+      </div>
+    </div>
+  );
+}
