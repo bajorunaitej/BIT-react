864c8a851c09b89a6d446411ee3aa675e606fa8f
bajorunaitej <bajorunaitej@gmail.com>
2024-02-28 21:15:26 +0200

    useMemo

--- my-scooter/src/components/Middle.jsx
+++ my-scooter/src/components/Middle.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useMemo } from "react";
 import { FaPencil } from "react-icons/fa6";
 import { FaTrashAlt } from "react-icons/fa";
+// import Button from "./Button";
 
 function Status({ status }) {
   return (
@@ -98,6 +99,12 @@ export default function Middle({ newScooter }) {
     return data;
   }
 
+  const handleDeleteScooter = (id) => {
+    const updatedScooters = scooter.filter((scooteris) => scooteris.id !== id);
+    setScooter(updatedScooters);
+    localStorage.setItem("scooters", JSON.stringify(updatedScooters));
+  };
+
   const filteredScooters = useMemo(
     () =>
       scooter.filter((val) => {
@@ -115,38 +122,33 @@ export default function Middle({ newScooter }) {
 
   // laisvi => uzimti => visi =>laisvi...
   // function getColor() {
-  // 	return showFreeScooters
-  // 		? "red"
-  // 		: showFreeScooters === false
-  // 		? "blue"
-  // 		: "green";
-  // }
-  // function getButtonText() {
-  // 	return showFreeScooters
-  // 		? "Rodyti užimtus"
-  // 		: showFreeScooters === false
-  // 		? "Rodyti visus"
-  // 		: "Rodyti laisvus";
+  //   return showFreeScooters
+  //     ? "red"
+  //     : showFreeScooters === false
+  //     ? "blue"
+  //     : "green";
   // }
 
-  const handleDeleteScooter = (id) => {
-    const updatedScooters = scooter.filter((scooteris) => scooteris.id !== id);
-    setScooter(updatedScooters);
-    localStorage.setItem("scooters", JSON.stringify(updatedScooters));
-  };
+  // function getButtonText() {
+  //   return showFreeScooters
+  //     ? "Rodyti užimtus"
+  //     : showFreeScooters === false
+  //     ? "Rodyti visus"
+  //     : "Rodyti laisvus";
+  // }
 
   return (
     <div className="container mx-auto bg-violet-100 min-h-[400px] flex flex-col gap-4 p-4">
       <div className="flex justify-center mt-28 gap-4">
         {/* <Button
-					text={getButtonText()}
-					color={getColor()}
-					onClick={() => {
-						setShowFreeScooters((prevValue) => {
-							return prevValue ? false : prevValue === false ? null : true;
-						});
-					}}
-				/> */}
+          text={getButtonText()}
+          color={getColor()}
+          onClick={() => {
+            setShowFreeScooters((prevValue) => {
+              return prevValue ? false : prevValue === false ? null : true;
+            });
+          }}
+        /> */}
         <select
           onChange={(e) => {
             if (e.target.value === "null") setShowFreeScooters(null);
