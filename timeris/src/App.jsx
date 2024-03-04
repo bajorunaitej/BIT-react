f5ad49ddb7706ad8e4575ce0c72d2c355c59c11b
bajorunaitej <bajorunaitej@gmail.com>
2024-03-04 08:17:08 +0200

    //

--- timeris/src/App.jsx
+++ timeris/src/App.jsx
@@ -6,19 +6,19 @@ import Stopwatch from "./components/Stopwatch";
 import { useState } from "react";
 
 function App() {
-  const [active, setActive] = useState("active1");
+  const [active, setActive] = useState("activeTimer");
 
   const handleActive = (activeClock) => {
     setActive(activeClock);
   };
 
   return (
-    <div className="container mx-auto flex flex-col justify-top items-center bg-[#4f1a586e] min-h-[200px] p-4 min-w-[350px]">
+    <div className="container mx-auto flex flex-col justify-center items-center bg-[#4f1a586e] min-h-[200px] p-4 min-w-[350px]">
       <div className="flex justify-between items-center w-[60%] gap-5">
         <button
-          onClick={() => handleActive("active1")}
+          onClick={() => handleActive("activeTimer")}
           className={`flex justify-evenly items-center p-2 gap-2 cursor-pointer border border-zinc-500 rounded ${
-            active === "active1" ? "bg-slate-500" : ""
+            active === "activeTimer" ? "bg-slate-500" : ""
           }`}
         >
           <span>
@@ -27,9 +27,9 @@ function App() {
           TIMER
         </button>
         <button
-          onClick={() => handleActive("active2")}
+          onClick={() => handleActive("activeStopwatch")}
           className={`flex justify-evenly items-center p-2 gap-2 cursor-pointer border border-zinc-500 rounded ${
-            active === "active2" ? "bg-slate-500" : ""
+            active === "activeStopwatch" ? "bg-slate-500" : ""
           }`}
         >
           <span>
@@ -39,10 +39,12 @@ function App() {
         </button>
       </div>
       <div>
-        <div className={`p-2 ${active === "active1" ? "block" : "hidden"}`}>
+        <div className={`${active === "activeTimer" ? "block" : "hidden"} p-1`}>
           <Timer />
         </div>
-        <div className={`p-2 ${active === "active2" ? "block" : "hidden"}`}>
+        <div
+          className={`${active === "activeStopwatch" ? "block" : "hidden"} p-1`}
+        >
           <Stopwatch />
         </div>
       </div>
