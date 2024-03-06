5d417ecc4161962286a285d6bb4b7d13ed2b62be
bajorunaitej <bajorunaitej@gmail.com>
2024-03-06 09:13:16 +0200

    d

--- PC_RENT_PLATFORM/index.js
+++ PC_RENT_PLATFORM/index.js
@@ -10,5 +10,15 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   console.log(err);
   if (err) console.log(err);
-  else console.log("Connected");
+  else {
+    console.log("Connected");
+    executeStatement();
+  }
 });
+
+async function executeStatement() {
+  const [result] = await connection
+    .promise()
+    .execute("SELECT * FROM `addresses` WHERE `id`=1");
+  console.log(result);
+}
