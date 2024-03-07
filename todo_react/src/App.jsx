c84fabe3dffd0b37e30f9663e78dee59ee8cef32
bajorunaitej <bajorunaitej@gmail.com>
2024-03-07 11:38:39 +0200

    add tasks

--- todo_react/src/App.jsx
+++ todo_react/src/App.jsx
@@ -1,7 +1,9 @@
-import "./App.css";
+import TodoList from "./TodoList";
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
 }
-
-export default App;
--- todo_react/src/TodoList.jsx
+++ todo_react/src/TodoList.jsx
@@ -1,3 +1,50 @@
+import { useState } from "react";
+
 export default function TodoList() {
-  return <div></div>;
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updateTasks = [...tasks];
+    updateTasks.splice(index, 1);
+    setTasks(updateTasks);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => {
+          setNewTask(e.target.value);
+        }}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((task, index) => {
+          return (
+            <li key={index}>
+              {task}
+              <button
+                onClick={() => {
+                  deleteTask(index);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
